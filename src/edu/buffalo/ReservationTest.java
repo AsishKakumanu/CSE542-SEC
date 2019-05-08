a4303e66a51c98280f5022bd7b515256ce0ecd16
Atrayee <atrayee.nag@gmail.com>
2019-05-08 14:47:08 -0400

    Junit changes

--- src/edu/buffalo/ReservationTest.java
+++ src/edu/buffalo/ReservationTest.java
@@ -21,48 +21,37 @@ import edu.buffalo.Student;
 public class ReservationTest {
 
 	ReservationGenerator revGen = new ReservationGenerator();
+	List<String> shuffledQuestionList = new ArrayList<String>();
+	List<String> shuffledTimeList = new ArrayList<String>();
+	List<String> shuffledNameList = new ArrayList<String>();
+	Hashtable<String, String> nameTable = new Hashtable<String, String>();
+	Queue<Reservation> reservationQueue = new LinkedList<Reservation>();
+	
 	@Test
-	public void testReservation() {
-		//fail("Not yet implemented");
-		int low = 2;
-		int high = 4;
-		List<String> shuffledQuestionList = new ArrayList<String>();
+	public void testReservationWhenQueueIsNotEmpty() {
+		int low = 0;
+		int high = 1;
 		
 		shuffledQuestionList.add("Where is Miki ?");
-		shuffledQuestionList.add("Where is Atrayee ?");
-		shuffledQuestionList.add("Where is Bhagyashree ?");
-		shuffledQuestionList.add("Where is Ashish ?");
-		shuffledQuestionList.add("Where is Yijiyang ?");
-		
-		List<String> shuffledTimeList = new ArrayList<String>();
 		shuffledTimeList.add("05/06/2019 14:12:58");
-		shuffledTimeList.add("05/06/2019 14:15:58");
-		shuffledTimeList.add("05/06/2019 14:16:58");
-		shuffledTimeList.add("05/06/2019 14:19:58");
-		shuffledTimeList.add("05/06/2019 14:20:58");
-		
-		List<String> shuffledNameList = new ArrayList<String>();
 		shuffledNameList.add("Miki");
-		shuffledNameList.add("Asish");
-		shuffledNameList.add("Atrayee");
-		shuffledNameList.add("Bhagyashree");
-		shuffledNameList.add("Yijiyang");
-		
-		
-		Hashtable<String, String> nameTable = new Hashtable<String, String>();
 		nameTable.put("Miki Padhiary", "mpadh@buffalo.edu");
-		nameTable.put("Atrayee Nag", "anag@buffalo.edu");
-		nameTable.put("Bhagyashree Throat", "bthroat@buffalo.edu");
-		nameTable.put("Ashish Kakumanyu", "akakumanyu@buffalo.edu");
-		nameTable.put("Yijiyang Wang", "ywang@buffalo.edu");
-		
-		Queue<Reservation> reservationQueue = new LinkedList<Reservation>();
 		
 		Reservation reservation = new Reservation(0, "05/06/2019 14:20:58", new Student("Miki", nameTable.get(0), null,  "Where is miki"));
 		reservationQueue.add(reservation);
-		revGen.generateReservations(low, high, shuffledQuestionList, shuffledTimeList, shuffledNameList, nameTable);
-		assertNotNull(reservationQueue);
 		
+		revGen.generateReservations(low, high, shuffledQuestionList, shuffledTimeList, shuffledNameList, nameTable);
+		assertEquals(reservationQueue.size(), 1);
+		
+	}
+	
+	
+	@Test
+	public void testReservationWhenQueueIsEmpty() {
+		int low = 0;
+		int high = 1;
+		revGen.generateReservations(low, high, shuffledQuestionList, shuffledTimeList, shuffledNameList, nameTable);
+		assertEquals(reservationQueue.size(), 0);
 	}
 
 }
