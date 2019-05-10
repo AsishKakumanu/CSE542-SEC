46e1332872d40345345cf04ac0571328b1b0a2a9
yijiangw <yijiang.wang@hotmail.com>
2019-05-10 17:21:15 -0400

    add cite info

--- AppointmentSys/src/View/QueDisplayPanel.java
+++ AppointmentSys/src/View/QueDisplayPanel.java
@@ -104,46 +104,30 @@ public class QueDisplayPanel extends JPanel {
 		
 		return true;
 	}
-	public void FitTableColumns(JTable myTable) {               //ÔOÖÃtableµÄÁÐŒ’ëSƒÈÈÝÕ{Õû
-
+	//cite from https://blog.csdn.net/tototuzuoquan/article/details/8982618
+	public void FitTableColumns(JTable myTable) {             
         JTableHeader header = myTable.getTableHeader();
-
         int rowCount = myTable.getRowCount();
-
         Enumeration columns = myTable.getColumnModel().getColumns();
 
         while (columns.hasMoreElements()) {
-
             TableColumn column = (TableColumn) columns.nextElement();
-
             int col = header.getColumnModel().getColumnIndex(
-
                     column.getIdentifier());
 
             int width = (int) myTable.getTableHeader().getDefaultRenderer()
-
                     .getTableCellRendererComponent(myTable,
-
                             column.getIdentifier(), false, false, -1, col)
-
                     .getPreferredSize().getWidth();
 
             for (int row = 0; row < rowCount; row++){
-
                 int preferedWidth = (int) myTable.getCellRenderer(row, col)
-
                         .getTableCellRendererComponent(myTable,
-
                                 myTable.getValueAt(row, col), false, false,
-
                                 row, col).getPreferredSize().getWidth();
-
                 width = Math.max(width, preferedWidth);
-
             }
-
             header.setResizingColumn(column);
-
             column.setWidth(width + myTable.getIntercellSpacing().width);
 
         }
