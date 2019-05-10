86cf153fb295820c2d97b9a0f8e53f7347ce607f
yijiangw <yijiang.wang@hotmail.com>
2019-05-10 16:17:46 -0400

    update test case

--- AppointmentSys/src/test/AbsentHandleTest.java
+++ AppointmentSys/src/test/AbsentHandleTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import Common.Appointment;
+import Common.BannedRecord;
 import Model.ModelDataManager;
 import View.MainFrame;
 
@@ -59,59 +60,106 @@ class AbsentHandleTest {
 	}
 
 	@Test
-	void banListAppendTest() {	
+	void checkEmptyBanListNotIncreaseWhenAbsentLongerThan10Min() {	
 		Date d = new Date();
-		int flag = 0;
-		if(d.getTime() - frame.getModelDataManager().getCurrentAppointment().getDate().getTime() <10*60000)
-			flag = 1;
-		frame.getModelDataManager().absenceFinishReport();
-		if(flag == 1)
-			assertNull(frame.getModelDataManager().getBanList());  // if attend in 10min won't be banned
-		else
-			assertEquals(frame.getModelDataManager().getAppointQueue().getBanList().size(),1); // if attend in too late banned
+		d = new Date(d.getTime() - 11*60000);
+		frame.getModelDataManager().getCurrentAppointment().setDate(d); //prepare the appointment time
+		assertNull(frame.getModelDataManager().getBanList());// the ban list should be empty at begin
+		
+		frame.getModelDataManager().absenceFinishReport();//do report
+		
+		assertNotNull(frame.getModelDataManager().getBanList());// if attend in too late banned		
+		assertEquals(frame.getModelDataManager().getAppointQueue().getBanList().size(),1); // the ban list should only have one record now
+	}
+	void checkNonEmptyBanListNotIncreaseWhenAbsentLongerThan10Min() {	
+		Date d = new Date();
+		d = new Date(d.getTime() - 11*60000);
+		frame.getModelDataManager().getCurrentAppointment().setDate(d); //prepare the appointment time
+		frame.getModelDataManager().getAppointQueue().getBanList().add(new BannedRecord("test@ub.edu", new Date()));// ban list should have one record now		
+		int preSize = frame.getModelDataManager().getAppointQueue().getBanList().size();
+		assertEquals(1,preSize);// check ban list have one record now
+		
+		frame.getModelDataManager().absenceFinishReport(); //do report
+		
+		assertNotNull(frame.getModelDataManager().getBanList());
+		assertNotEquals(preSize,frame.getModelDataManager().getAppointQueue().getBanList().size()); // the ban list should have two record now
+		assertEquals(2,frame.getModelDataManager().getAppointQueue().getBanList().size()); // the ban list should have two record now
+
 	}
 	@Test
-	void queueRemoveTest() {
+	void checkEmptyBanListWronglyIncreaseWhenAbsentWhithin10Min() {	
 		Date d = new Date();
-		int flag = 0;
-		int len =frame.getModelDataManager().getAppointQueue().getQueue().size();		
-//		System.out.println("len = "+len);
-		if(d.getTime() - frame.getModelDataManager().getCurrentAppointment().getDate().getTime() <10*60000)
-			flag = 1;
-		frame.getModelDataManager().absenceFinishReport();
-		if(flag == 1)
-			len = len+1;	// queue len increase one(re-Add) if attend in 10min late 
-//		System.out.println("len 2 = "+len);
-//		System.out.println("size = "+frame.getModelDataManager().getAppointQueue().getQueue().size());
-		assertEquals(len,frame.getModelDataManager().getAppointQueue().getQueue().size()); // check the queue len if attend too late the len won't add one
+		d = new Date(d.getTime() - 5*60000);
+		frame.getModelDataManager().getCurrentAppointment().setDate(d); //prepare the appointment time
+		int preSize = frame.getModelDataManager().getAppointQueue().getBanList().size();
+		assertNull(frame.getModelDataManager().getBanList());// the ban list should be empty at begin
+
+		frame.getModelDataManager().absenceFinishReport();//do report
+		
+		assertNull(frame.getModelDataManager().getBanList());// the ban list should still empty now
+		assertNotEquals(frame.getModelDataManager().getAppointQueue().getBanList().size(),preSize+1); 
+		assertEquals(frame.getModelDataManager().getAppointQueue().getBanList().size(),preSize); 
+	}
+	@Test
+	void checkNonEmptyBanListWronglyIncreaseWhenAbsentWhithin10Min() {	
+		Date d = new Date();
+		d = new Date(d.getTime() - 5*60000);
+		frame.getModelDataManager().getCurrentAppointment().setDate(d); //prepare the appointment time
+		frame.getModelDataManager().getAppointQueue().getBanList().add(new BannedRecord("test@ub.edu", new Date()));// ban list should have one record now		
+		int preSize = frame.getModelDataManager().getAppointQueue().getBanList().size();
+		assertEquals(1,preSize);// check ban list have one record now
+		frame.getModelDataManager().absenceFinishReport();//do report
+		assertNotNull(frame.getModelDataManager().getBanList());
+		assertEquals(preSize,frame.getModelDataManager().getAppointQueue().getBanList().size()); // the ban list should still have one record now
+	}
+	@Test
+	void checkQueueNotReAddWhenAbsentWhithin10Min() {
+		Date d = new Date();
+		d = new Date(d.getTime() - 5*60000);	
+		frame.getModelDataManager().getCurrentAppointment().setDate(d); //prepare the appointment time
+		int len =frame.getModelDataManager().getAppointQueue().getQueue().size();//before report the queue len will pre-minus one by poll to process list
+		
+		frame.getModelDataManager().absenceFinishReport();//do report
+		
+		assertNotEquals(len,frame.getModelDataManager().getAppointQueue().getQueue().size()); // test queue len not same as begin 
+		assertEquals(len + 1,frame.getModelDataManager().getAppointQueue().getQueue().size()); // queue len increase one(re-Add) when attend in 10min late 
 	}	
 	@Test
-	void queueReAddTest() {
+	void checkQueueReAddWhenAbsentLongerThan10Min() {
 		Date d = new Date();
-		int flag = 0;
-		int len =frame.getModelDataManager().getAppointQueue().getQueue().size();
+		d = new Date(d.getTime() - 11*60000);		
+		frame.getModelDataManager().getCurrentAppointment().setDate(d); //prepare the appointment time
+		int len =frame.getModelDataManager().getAppointQueue().getQueue().size();//before report the queue len will pre-minus one by poll to process list
+		
+		frame.getModelDataManager().absenceFinishReport();//do report
+		
+		assertNotEquals(len + 1,frame.getModelDataManager().getAppointQueue().getQueue().size()); // test queue len shuold not increase one
+		assertEquals(len,frame.getModelDataManager().getAppointQueue().getQueue().size()); // test queue should same as begin the appointment just removed from processing list
+	}	
+	@Test
+	void checkQueueElemReAddCorrectly() {
+		Date d = new Date();
+		d = new Date(d.getTime() - 5*60000);	
+		frame.getModelDataManager().getCurrentAppointment().setDate(d); //prepare the appointment time		
 		int curID = frame.getModelDataManager().getCurrentAppointment().getID();
-//		System.out.println("curID = "+curID);
-		if(d.getTime() - frame.getModelDataManager().getCurrentAppointment().getDate().getTime() <10*60000)
-			flag = 1;
-		frame.getModelDataManager().absenceFinishReport();
+
+		frame.getModelDataManager().absenceFinishReport(); //do report
+		
 		Appointment lastAppointment = null;
-		// get the tail element in the queue which is re-Add if in 10min late, it will be the last report Appointment ID
+		// get the tail element in the queue which is re-Add if in 10min late, it should be the last report Appointment ID
 		for (Appointment x : frame.getModelDataManager().getAppointQueue().getQueue()) { 
 			lastAppointment = x;   			
         } 
-		if(flag == 1)
-			assertEquals(curID,lastAppointment.getID()); //compare the last report id and the tail appointment id
-		else
-			assertEquals(frame.getModelDataManager().getAppointQueue().getQueue().size(),len); // check the queue len if attend too late the len won't add one
 		
+		assertEquals(curID,lastAppointment.getID()); //compare the last report id and the tail appointment id
 	}	
 	@Test
 	void reportWhenEmptyTest() {
 		int curID = frame.getModelDataManager().getCurrentAppointment().getID();
 		int response0 = frame.getModelDataManager().getAppointQueue().absenceHandle(curID);
-		int response = frame.getModelDataManager().getAppointQueue().absenceHandle(curID); // report same ID twice, already process once list empty now
 		assertEquals(0,response0); // success report
+		
+		int response = frame.getModelDataManager().getAppointQueue().absenceHandle(curID); // report same ID twice, already process once list empty now
 		assertEquals(1,response);  // response the empty Processing Appointment List error code
 	}
 	@Test
